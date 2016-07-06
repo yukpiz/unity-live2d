5e76ad74815fd8bc408eea7be4000a787eef1490
yukpiz <yukpiz@gmail.com>
2016-07-06 17:27:11 +0900

    Updated script. * Blink eye. * Saving scene file.

--- Assets/Scripts/StandModel.cs
+++ Assets/Scripts/StandModel.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using live2d;
+using live2d.framework;
 
-public class StandModel : MonoBehaviour {
+public class StandModel : MonoBehaviour
+{
+	public TextAsset mocFile; //モデルファイル
+	public Texture2D[] textureFiles; //テクスチャファイル
 
-	// Use this for initialization
-	void Start () {
-	
+	private Live2DModelUnity live2DModel;
+	private EyeBlinkMotion eyeBlink = new EyeBlinkMotion();
+
+	public void Start()
+	{
+		Live2D.init (); //Live2Dの初期化
+		//mocファイルの読み込み
+		live2DModel = Live2DModelUnity.loadModel (mocFile.bytes);
+
+		//テクスチャファイルの読み込み
+		int index = 0;
+		foreach (Texture2D textureFile in textureFiles) {
+			live2DModel.setTexture (index, textureFile);
+			index++;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void Update()
+	{
+		float modelWidth = live2DModel.getCanvasWidth ();
+		Matrix4x4 m1 = Matrix4x4.Ortho(0, modelWidth, modelWidth, 0, -50.0f, 50.0f);
+		Matrix4x4 m2 = transform.localToWorldMatrix;
+		Matrix4x4 m3 = m2 * m1;
+
+		live2DModel.setMatrix (m3);
+
+		eyeBlink.setParam (live2DModel);
+		live2DModel.update ();
+
+	}
+
+	public void OnRenderObject()
+	{
+		live2DModel.draw ();
 	}
 }
